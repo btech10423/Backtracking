611d44797c3fc01bc04f6715ec6f711b474daa7e
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-16 20:29:10 +0530

    Add files via upload

---
+++ Gridways.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Gridways {
+    public static int ways(int i,int j,int n, int m){
+        if(i==n-1 && j==m-1){
+            return 1;
+        }
+        else if(i==n || j==m){
+            return 0;
+        }
+        int w1=ways(i+1, j, n,  m);
+        int w2=ways(i, j+1, n,  m);
+        return w1+w2;
+    }
+     public static void main(String args[]) {
+        int n=3,m=3;
+        System.out.print(ways(0,0, n, m));
+    }
+}
